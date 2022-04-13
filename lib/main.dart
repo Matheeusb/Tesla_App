d2dfbbb6b68b288e48c329b287f1596cda50bb60
Matheus Bereta <matheus_bereta@hotmail.com>
2022-04-13 14:18:24 -0300

    Add Device Preview lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tesla_app/screens/home_screen.dart';
 
 import 'screens/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -16,6 +21,9 @@ class MyApp extends StatelessWidget {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Tesla App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
